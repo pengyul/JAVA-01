8101a411aa0e6db221dc4f33cfcea37d2c1b1167
pengyulong <762309526@qq.com>
2021-01-14 10:38:27 +0800

    week01 作业

--- Week_01/README.md
+++ Week_01/README.md
@@ -1,1 +1,3 @@
-学习笔记
+第一课作业实践：
+第2题：java/MyClassLoader.java
+第3题：img.png
---
+++ Week_01/java/MyClassLoader.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MyClassLoader extends ClassLoader{
+    public static void main(String[] args) {
+        try {
+            Class clazz = new MyClassLoader().findClass("Hello");
+            if(clazz != null) {
+                Object object = clazz.newInstance();
+                Method method = clazz.getMethod("hello");
+                method.invoke(object);
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Path path = Paths.get("Week_01/Hello.xlass");
+        try {
+            byte[] data = Files.readAllBytes(path); //获取文件所有字节
+            data = convert(data);
+            return this.defineClass("Hello", data, 0, data.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 字节数组每个字节取反转换
+     * @param bytes 原数组
+     * @return 取反数组
+     */
+    private byte[] convert(byte[] bytes){
+        for (int i =0; i < bytes.length; i++){
+            bytes[i] = (byte)~bytes[i];
+        }
+        return bytes;
+    }
+}
